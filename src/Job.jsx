464051c5150e7d6e5a954a3a5520bb115bdc336d
Loquen Jones <lrjones416@gmail.com>
2020-04-30 09:45:38 -0700

    rendering all jobs from data.json

--- src/Job.jsx
+++ src/Job.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
 
-function Job(props) {
+function Job({
+  company, url, newJob, featured, position,
+  role, level, postedAt, contract, location,
+  languages, tools
+}) {
+  let filters = [role, level, ...languages, ...tools];
   return (
-    <div>
-      Job
+    <div className='job'>
+      <div className='logo'>
+        <img src={url} alt={`${company} logo`}/>
+      </div>
+      <div className='company'>
+        <div className='title'>{company}</div>
+        <div className='new'>New!</div>
+        <div className='feature'>Featured</div>
+      </div>
+      <div className='position'>
+        {position}
+      </div>
+      <div className='about'>
+        <div className='time'>{postedAt}</div>
+        <div className='type'>{contract}</div>
+        <div className='location'>{location}</div>
+      </div>
+      {filters.map((item, key) => 
+        <div key={key} className='filter-option'>
+          {item}
+        </div>
+        )}
     </div>
   );
 }
--- src/JobList.jsx
+++ src/JobList.jsx
@@ -5,13 +5,27 @@ let jobs = require('./data.json');
 
 function JobList(props) {
   return (
-    <>
+    <div className='job-list'>
       {
         jobs.map(job => 
-          <Job/>
+          <Job
+            key={job.id}
+            company={job.company}
+            url={job.logo}
+            newJob={job.new}
+            featured={job.featured}
+            position={job.position}
+            role={job.role}
+            level={job.level}
+            postedAt={job.postedAt}
+            contract={job.contract}
+            location={job.location}
+            languages={job.languages ? job.languages : []}
+            tools={job.tools ? job.tools : []}
+          />
         )
       }
-    </>
+    </div>
   );
 }
 

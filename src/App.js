02f2b3226f33cacfc1dd2a3b84b5e7f7a343b08d
Loquen Jones <lrjones416@gmail.com>
2020-05-01 16:43:42 -0700

    filter works for most options

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Filter from './Filter';
 import JobList from './JobList';
 import './App.scss';
@@ -22,6 +22,62 @@ function App() {
 
     setFilterList(currentFilter);
   }
+
+  const createJobOptions = () => {
+    let updatedJobs = jobs;
+    updatedJobs.forEach(job => {
+      job.tools = job.tools ? job.tools : [];
+      job.languages = job.languages ? job.languages : [];
+      let optionsList = [
+        job.role, 
+        job.level, 
+        ...job.languages, 
+        ...job.tools
+      ];
+
+      updatedJobs.optionsList = optionsList;
+    });
+
+    setJobs(updatedJobs);
+  }
+
+  useEffect(() => {
+    if(filterList.size > 0) {
+      const filteredJobs = [];  
+      jobs.forEach(job => {
+        job.tools = job.tools ? job.tools : [];
+        job.languages = job.languages ? job.languages : [];
+        let optionsList = [
+          job.role, 
+          job.level, 
+          ...job.languages, 
+          ...job.tools
+        ];
+
+        filterList.forEach(option => {
+          if(optionsList.includes(option) && !filteredJobs.includes(job)){
+            filteredJobs.push(job);
+          } else {
+            console.log(option, optionsList);
+            const index = filteredJobs.indexOf(option);
+            if (index > -1) {
+              filteredJobs.splice(index, 1);
+            }
+          }
+        });
+      });
+
+    // https://stackoverflow.com/questions/34901593/how-to-filter-an-array-from-all-elements-of-another-array
+
+    //   var arr = [1,2,3,4],
+    // brr = [2,4],
+    // res = arr.filter(f => !brr.includes(f));
+
+      setJobs(filteredJobs);
+    } else {
+      setJobs(data);
+    }
+  }, [filterList]);
 
   return (
     <div className="App">

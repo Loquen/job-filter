ad2003dde487d12633560faa7227f1970c359520
Loquen Jones <lrjones416@gmail.com>
2020-05-04 10:40:26 -0700

    add arrayContainsArray method to build out filtered jobs list

--- src/App.js
+++ src/App.js
@@ -23,22 +23,10 @@ function App() {
     setFilterList(currentFilter);
   }
 
-  const createJobOptions = () => {
-    let updatedJobs = jobs;
-    updatedJobs.forEach(job => {
-      job.tools = job.tools ? job.tools : [];
-      job.languages = job.languages ? job.languages : [];
-      let optionsList = [
-        job.role, 
-        job.level, 
-        ...job.languages, 
-        ...job.tools
-      ];
+  function arrayContainsArray (superset, subset) {
+    if (subset.size === 0) return false;
 
-      updatedJobs.optionsList = optionsList;
-    });
-
-    setJobs(updatedJobs);
+    return subset.every((value) => (superset.indexOf(value) >= 0));
   }
 
   useEffect(() => {
@@ -54,24 +42,17 @@ function App() {
           ...job.tools
         ];
 
-        filterList.forEach(option => {
-          if(optionsList.includes(option) && !filteredJobs.includes(job)){
-            filteredJobs.push(job);
-          } else {
-            console.log(option, optionsList);
-            const index = filteredJobs.indexOf(option);
-            if (index > -1) {
-              filteredJobs.splice(index, 1);
-            }
+        let subset = [...filterList];
+        if(arrayContainsArray(optionsList, subset)) {
+          filteredJobs.push(job);
+        } else {
+          const index = filteredJobs.indexOf(job);
+    
+          if (index > -1) {
+            filteredJobs.splice(index, 1);
           }
-        });
+        }
       });
-
-    // https://stackoverflow.com/questions/34901593/how-to-filter-an-array-from-all-elements-of-another-array
-
-    //   var arr = [1,2,3,4],
-    // brr = [2,4],
-    // res = arr.filter(f => !brr.includes(f));
 
       setJobs(filteredJobs);
     } else {
